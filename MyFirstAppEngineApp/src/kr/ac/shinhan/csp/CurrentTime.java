71e441dd298d1cc2f3f026db48c513b8099da6a0
jaewoo <jaewoo@jaewoo-PC>
2015-03-20 12:34:08 +0900

    최초의커밋

---
+++ MyFirstAppEngineApp/src/kr/ac/shinhan/csp/CurrentTime.java
@@ -0,0 +1,19 @@
+package kr.ac.shinhan.csp;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class CurrentTime extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.setCharacterEncoding("UTF-8");
+		resp.getWriter().println("현재시간 : " + new Date());
+	}
+}
+//http://shcjaewoo.appspot.com/CurrentTime
